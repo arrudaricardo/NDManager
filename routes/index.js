f950af59cba467c6dd76619ec2de8333b5c06248
JongIn <jiku90@naver.com>
2017-05-04 16:41:49 +0900

    [UPDATE]

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const storageFolder = path.resolve('/Volumes/Seagate Backup Plus Drive');
+  const storageFolder = path.resolve('/Volumes/Seagate Backup Plus Drive/Webtoon');
   fs.readdir(storageFolder, (err, files) => {
     if (err) {
       res.send(err);
